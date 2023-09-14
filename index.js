66c3aa16d68526723e99e9697bdd6e800f26594e
Hamdanirevou <hamdanie100103@gmail.com>
2023-09-14 20:55:14 +0700

    push todolist app

---
+++ index.js
@@ -0,0 +1,19 @@
+//reference html element in js
+const formTask = document.querySelector(".form-task")
+
+formTask.addEventListener("submit", () => {
+    event.preventDefault();
+//reference html element input 
+    const inputform = document.querySelector(".input-form");
+    console.log(inputform.value);
+
+    //reference html element ul/ wrapper list
+    const wrapperList =document.querySelector(".task-list-wrapper");
+
+    //create document li
+    const taskList = document.createElement("li");
+    taskList.innerHTML = inputform.value;
+
+    //appen li ke ul 
+    wrapperList.append(taskList);
+});
